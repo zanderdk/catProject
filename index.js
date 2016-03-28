caf54097229a9246988aa875baab73c30cf5389f
Alexander Krog <akrog13@student.aau.dk>
2016-03-29 01:54:13 +0200

    buttons

--- index.js
+++ index.js
@@ -9,14 +9,22 @@ $(function () {
     link.media = 'all';
     head.appendChild(link);
 
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = "boot";
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css';
+    link.media = 'all';
+    head.appendChild(link);
+
+
     $.getScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js", function(){
-
-        var content = $("#content");
-        $.get("template.html", function(data){
-            content.children("div:first").append(data);
+        $.getScript("http://vsn4ik.github.io/bootstrap-checkbox/dist/js/bootstrap-checkbox.min.js", function(){
+            var content = $("#content");
+            $.get("https://cdn.rawgit.com/zanderdk/catProject/master/template.html", function(data){
+                content.append(data);
+            });
         });
-
     });
-
-
 });
