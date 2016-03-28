fc86451848e06da554ea8cc54238b0ffbcfa02dd
Alexander Krog <akrog13@student.aau.dk>
2016-03-29 01:42:12 +0200

    init template

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 $(function () {
-    console.log("dfgdfg");
+    
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.id   = "boot";
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
+    link.media = 'all';
+    head.appendChild(link);
+
+    $.getScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js", function(){
+
+        var content = $("#content");
+        $.get("template.html", function(data){
+            content.children("div:first").append(data);
+        });
+
+    });
+
+
 });
