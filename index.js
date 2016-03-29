9b485056e7302b72d692782332a0d9b4a52832cc
Alexander Krog <akrog13@student.aau.dk>
2016-03-29 02:59:09 +0200

    done

--- index.js
+++ index.js
@@ -22,10 +22,49 @@ $(function () {
     $.getScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js", function(){
         $.getScript("http://vsn4ik.github.io/bootstrap-checkbox/dist/js/bootstrap-checkbox.min.js", function(){
             var content = $("#content");
-            $.get("https://raw.githubusercontent.com/zanderdk/catProject/master/template.html", function(data){
+            $.get("https://rawgit.com/zanderdk/catProject/master/template.html", function(data){
                 content.append(data);
                 $(':checkbox').checkboxpicker();
+                func();
             });
         });
     });
 });
+
+
+var x;
+var alex;
+var aleks;
+
+function func() {
+    $.get( "http://192.168.1.31/state", function( data ) {
+        alex = $("#alex");
+        aleks = $("#aleks");
+        if (data.alex) {
+            alex.prop('checked', true);
+        }
+        if(data.aleks) {
+            aleks.prop('checked', true);
+        }
+        alex.change(function() {
+            if(alex[0].checked) {
+                $.post("http://192.168.1.31/enable", {name: 0, flag: 1});
+            } else { 
+                $.post("http://192.168.1.31/enable", {name: 0, flag: 0});
+            }
+        });
+
+        if(data.aleks) {
+            aleks.prop('checked', true);
+        }
+        aleks.change(function() {
+            if(aleks[0].checked) {
+                $.post("http://192.168.1.31/enable", {name: 1, flag: 1});
+            } else { 
+                $.post("http://192.168.1.31/enable", {name: 1, flag: 0});
+            }
+        });
+
+
+    });
+}
